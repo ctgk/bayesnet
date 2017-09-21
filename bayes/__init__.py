40278fc8f105cf117c18974c871ff720ee54bb23
ctgk <r0735nj5058@icloud.com>
2017-09-21 22:27:33 +0900

    installation

---
+++ bayes/__init__.py
@@ -0,0 +1,3 @@
+from bayes import (
+    tensor
+)
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+
+setup(
+    name="bayes",
+    version="0.0.1",
+    packages=["bayes"],
+    test_suite="test"
+)
