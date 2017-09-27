c0570010d44cf2b7d84a5f0b792f776e5386d15b
ctgk <r0735nj5058@icloud.com>
2017-09-27 18:41:10 +0900

    added methods to compute loss and set random variables

--- bayesnet/network.py
+++ bayesnet/network.py
@@ -1,9 +1,26 @@
+from bayesnet.random.random import RandomVariable
 from bayesnet.tensor.parameter import Parameter
 
 
 class Network(object):
+    """
+    a base class for network building
+
+    Parameters
+    ----------
+    kwargs : tensor_like
+        parameters to be optimized
+
+    Attributes
+    ----------
+    parameter : dict
+        dictionary of parameters to be optimized
+    random_variable : dict
+        dictionary of random varibles
+    """
 
     def __init__(self, **kwargs):
+        self.random_variable = {}
         self.parameter = {}
         for key, value in kwargs.items():
             if isinstance(value, Parameter):
@@ -16,6 +33,21 @@ class Network(object):
                 self.parameter[key] = value
             object.__setattr__(self, key, value)
 
+    def __setattr__(self, key, value):
+        if isinstance(value, RandomVariable):
+            if value.data is not None or value.prior is not None:
+                self.random_variable[key] = value
+        object.__setattr__(self, key, value)
+
     def cleargrad(self):
         for p in self.parameter.values():
             p.cleargrad()
+
+    def loss(self):
+        loss = 0
+        for rv in self.random_variable.values():
+            if rv.observed:
+                loss += -rv.log_pdf().sum()
+            else:
+                loss += rv.KLqp().sum()
+        return loss
