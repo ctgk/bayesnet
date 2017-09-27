dce51fb947720f594e1efd21e7243ad90ecef726
ctgk <r0735nj5058@icloud.com>
2017-09-27 18:42:44 +0900

    examples

--- example/linear_regression.py
+++ example/linear_regression.py
@@ -8,11 +8,13 @@ class LinearRegressor(bn.Network):
     def __init__(self, n_input, n_output):
         super().__init__(
             w=np.zeros((n_input, n_output)),
-            b=np.zeros(n_output)
+            b=np.zeros(n_output),
+            logs=1
         )
 
-    def __call__(self, x):
-        return x @ self.w + self.b
+    def __call__(self, x, y=None):
+        self.y = bn.random.Gaussian(x @ self.w + self.b, bn.exp(self.logs), data=y)
+        return self.y.mu
 
 
 def main():
@@ -20,19 +22,21 @@ def main():
     y_train = 5 * x_train - 1 + np.random.normal(scale=1., size=x_train.shape)
 
     model = LinearRegressor(1, 1)
-    optimizer = bn.optimizer.GradientDescent(model.parameter, 0.1)
-    for _ in range(1000):
+    optimizer = bn.optimizer.GradientDescent(model, 0.1)
+    for i in range(1000):
         model.cleargrad()
-        y = model(x_train)
-        loss = bn.mean_squared_error(y, y_train)
+        model(x_train, y_train)
+        loss = model.loss()
         loss.backward()
         optimizer.update()
+        if i % 100 == 0:
+            print(loss.value)
 
     x = np.linspace(-1, 1, 100)[:, None]
-    y = model(x).value
+    y = model(x)
     plt.scatter(x_train, y_train, marker="x")
     plt.plot(
-        x, y, color="orange",
+        x, y.value, color="orange",
         label=f"{model.w.value.item():.2}x+{model.b.value.item():.2}"
     )
     plt.legend()
