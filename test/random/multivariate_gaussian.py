bc69cafcaa1a8de02482aff0948c0ca3831a942c
ctgk <r0735nj5058@icloud.com>
2017-09-30 12:15:17 +0900

    test backprop of multivariate gaussian

--- test/random/multivariate_gaussian.py
+++ test/random/multivariate_gaussian.py
@@ -9,6 +9,24 @@ class TestMultivariateGaussian(unittest.TestCase):
         self.assertRaises(ValueError, bn.random.MultivariateGaussian, np.zeros(2), np.eye(3))
         self.assertRaises(ValueError, bn.random.MultivariateGaussian, np.zeros(2), np.eye(2) * -1)
 
+        x_train = np.array([
+            [1., 1.],
+            [1., -1],
+            [-1., 1.],
+            [-1., -2.]
+        ])
+        mu = bn.Parameter(np.ones(2))
+        cov = bn.Parameter(np.eye(2) * 2)
+        optimizer = bn.optimizer.GradientDescent([mu, cov], 0.1)
+        for _ in range(1000):
+            optimizer.cleargrad()
+            x = bn.random.MultivariateGaussian(mu, cov + cov.transpose(), data=x_train)
+            loss = -x.log_pdf().sum()
+            loss.backward()
+            optimizer.update()
+        self.assertTrue(np.allclose(mu.value, x_train.mean(axis=0)))
+        self.assertTrue(np.allclose(np.cov(x_train, rowvar=False, bias=True), x.cov.value))
+
         g = bn.random.MultivariateGaussian(
             np.ones(2), np.eye(2) * 2
         )
