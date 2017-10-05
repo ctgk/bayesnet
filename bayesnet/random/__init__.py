4ebe7a994bd4d8db3623278e43935d6fd3319bc3
ctgk <r0735nj5058@icloud.com>
2017-10-05 16:40:32 +0900

    implemented mixture of gaussian

--- bayesnet/random/__init__.py
+++ bayesnet/random/__init__.py
@@ -1,6 +1,7 @@
 from bayesnet.random.bernoulli import Bernoulli
 from bayesnet.random.exponential import Exponential
 from bayesnet.random.gaussian import Gaussian
+from bayesnet.random.gaussian_mixture import GaussianMixture
 from bayesnet.random.multivariate_gaussian import MultivariateGaussian
 
 
@@ -8,5 +9,6 @@ __all__ = [
     "Bernoulli",
     "Exponential",
     "Gaussian",
+    "GaussianMixture",
     "MultivariateGaussian"
 ]
