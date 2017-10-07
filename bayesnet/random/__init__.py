002aa184c5dd93375eff457794481f09ec3f17ed
ctgk <r0735nj5058@icloud.com>
2017-10-07 16:02:04 +0900

    implemented categorical distribution

--- bayesnet/random/__init__.py
+++ bayesnet/random/__init__.py
@@ -1,4 +1,5 @@
 from bayesnet.random.bernoulli import Bernoulli
+from bayesnet.random.categorical import Categorical
 from bayesnet.random.exponential import Exponential
 from bayesnet.random.gaussian import Gaussian
 from bayesnet.random.gaussian_mixture import GaussianMixture
@@ -7,6 +8,7 @@ from bayesnet.random.multivariate_gaussian import MultivariateGaussian
 
 __all__ = [
     "Bernoulli",
+    "Categorical",
     "Exponential",
     "Gaussian",
     "GaussianMixture",
