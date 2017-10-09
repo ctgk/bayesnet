fb7e0c8f4847c22792756e2542fd22bf658e0f9f
ctgk <r0735nj5058@icloud.com>
2017-10-09 14:29:03 +0900

    added some optimizers

--- bayesnet/optimizer/__init__.py
+++ bayesnet/optimizer/__init__.py
@@ -1,8 +1,16 @@
+from bayesnet.optimizer.ada_delta import AdaDelta
+from bayesnet.optimizer.ada_grad import AdaGrad
 from bayesnet.optimizer.adam import Adam
 from bayesnet.optimizer.gradient_ascent import GradientAscent
+from bayesnet.optimizer.momentum import Momentum
+from bayesnet.optimizer.rmsprop import RMSProp
 
 
 __all__ = [
+    "AdaDelta",
+    "AdaGrad",
     "Adam",
-    "GradientAscent"
+    "GradientAscent",
+    "Momentum",
+    "RMSProp"
 ]
