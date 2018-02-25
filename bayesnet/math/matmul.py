f4f76972b000fdad47d08f9e0af1ef058a35d8cb
ctgk <r0735nj5058@icloud.com>
2018-02-25 20:46:34 +0900

    matrix multiplication for multi-dimensional array

--- bayesnet/math/matmul.py
+++ bayesnet/math/matmul.py
@@ -1,3 +1,5 @@
+import numpy as np
+from bayesnet.array.broadcast import broadcast_to
 from bayesnet.tensor.constant import Constant
 from bayesnet.tensor.tensor import Tensor
 from bayesnet.function import Function
@@ -11,13 +13,19 @@ class MatMul(Function):
     def _check_input(self, x, y):
         x = self._convert2tensor(x)
         y = self._convert2tensor(y)
-        self._equal_ndim(x, 2)
-        self._equal_ndim(y, 2)
-        if x.shape[1] != y.shape[0]:
+        self._atleast_ndim(x, 2)
+        self._atleast_ndim(y, 2)
+        if x.shape[-1] != y.shape[-2]:
             raise ValueError(
-                "shapes {} and {} not aligned: {} (dim 1) != {} (dim 0)"
-                .format(x.shape, y.shape, x.shape[1], y.shape[0])
+                "shapes {} and {} not aligned: {} (dim -1) != {} (dim -2)"
+                .format(x.shape, y.shape, x.shape[-1], y.shape[-2])
             )
+        if x.shape[:-2] != y.shape[:-2]:
+            shape = np.broadcast(x.value[..., 0, 0], y.value[..., 0, 0]).shape
+            if x.shape[:-2] != shape:
+                x = broadcast_to(x, shape + x.shape[-2:])
+            if y.shape[:-2] != shape:
+                y = broadcast_to(y, shape + y.shape[-2:])
         return x, y
 
     def forward(self, x, y):
@@ -29,8 +37,8 @@ class MatMul(Function):
         return Tensor(x.value @ y.value, function=self)
 
     def backward(self, delta):
-        dx = delta @ self.y.value.T
-        dy = self.x.value.T @ delta
+        dx = delta @ np.swapaxes(self.y.value, -1, -2)
+        dy = np.swapaxes(self.x.value, -1, -2) @ delta
         self.x.backward(dx)
         self.y.backward(dy)
 
